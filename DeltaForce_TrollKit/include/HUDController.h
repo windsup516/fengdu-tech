5cbdc163a7f48ddbad5ff9aed3615eb74c5f9e78
windsup516 <fanxiaoyu6F@163.com>
2026-05-27 18:23:22 +0800

    Add files via upload

---
+++ DeltaForce_TrollKit/include/HUDController.h
@@ -0,0 +1,27 @@
+#ifndef HUD_CONTROLLER_H
+#define HUD_CONTROLLER_H
+
+#import <UIKit/UIKit.h>
+
+@class HUDMainWindow, TouchMainWindow, HUDRootViewController, TouchViewController;
+
+@interface HUDController : NSObject
+
+@property (nonatomic, strong) HUDMainWindow *hudWindow;
+@property (nonatomic, strong) TouchMainWindow *touchWindow;
+@property (nonatomic, strong) HUDRootViewController *rootVC;
+@property (nonatomic, strong) TouchViewController *touchVC;
+@property (nonatomic, strong) id hostingController;
+@property (nonatomic) BOOL showing;
+@property (nonatomic) BOOL windowsCreated;
+
++ (instancetype)shared;
+- (void)show;
+- (void)hide;
+- (void)createWindowsOnScene:(id)scene;
+- (void)registerHIDEventCallback;
+- (void)syncTouchWindowToPanel;
+- (void)setupHostingController;
+
+@end
+#endif
